d695d7b41bef96cf40f21049c0be7ae3e0d3f9a4
Weellyyy <wildanprtm2304@gmail.com>
2024-05-28 18:57:23 +0700

    menambahkan int main pada abstraksi

--- Abstraksi/Abstraksi.cpp
+++ Abstraksi/Abstraksi.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 
-class AbstraksiKlas{
+class AbstraksiKlas {
 private: string x, y;
 
 public:
@@ -18,5 +18,12 @@ public:
 		cout << "x = " << x << endl;
 		cout << "y = " << y << endl;
 	}
-}
+};
 
+int main() {
+	AbstraksiKlas ak;
+	ak.setXY("Yogyakarta", "Kampus");
+	ak.display();
+
+	return 0;
+}
