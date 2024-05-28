89cc02a239835c0b2d45fafcae0a9b995e744757
Weellyyy <wildanprtm2304@gmail.com>
2024-05-28 18:41:34 +0700

    menambahkan dervide class sebagai turunan dari base class

--- noOveridding/noOveridding.cpp
+++ noOveridding/noOveridding.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 class baseClass {
-
+public:
+	virtual void perkenalan() {
+		cout << "Hallo saya Function dari base class";
+	}
+};
+class derivedClass : public baseClass {
+public:
+	void perkenalan() {
+		cout << "Hallo saya Function dari derived class";
+	}
 };
